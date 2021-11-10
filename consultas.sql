be9647e6a8d6b8793dfcf173aab140ad36ad1149
Pedro Guzman <my.rg.developer@gmail.com>
2021-11-09 23:10:06 -0600

    Se agrego archivo consulta.sql

---
+++ consultas.sql
@@ -0,0 +1,31 @@
+-- OBTENER VENTAS A CREDITO
+select v.id_venta, v.total_venta, CONCAT(c.nombres, ' ',c.aPaterno, ' ', c.aMaterno) as cliente,
+    CASE
+        WHEN v.credito THEN 'SI'
+        ELSE 'NO'
+    END 
+    AS credito
+from ventas v
+inner join clientes c on c.id_cliente = v.id_cliente
+where v.credito = true;
+
+-- OBTENER VENTAS DE CONTADO
+select v.id_venta, v.total_venta, CONCAT(c.nombres, ' ',c.aPaterno, ' ', c.aMaterno) as cliente,
+    CASE
+        WHEN v.credito THEN 'SI'
+        ELSE 'NO'
+    END 
+    AS credito
+from ventas v
+inner join clientes c on c.id_cliente = v.id_cliente
+where v.credito = false;
+
+-- OBTENER VENTAS TODAS
+select v.id_venta, v.total_venta, CONCAT(c.nombres, ' ',c.aPaterno, ' ', c.aMaterno) as cliente,
+    CASE
+        WHEN v.credito THEN 'SI'
+        ELSE 'NO'
+    END 
+    AS credito
+from ventas v
+inner join clientes c on c.id_cliente = v.id_cliente;
